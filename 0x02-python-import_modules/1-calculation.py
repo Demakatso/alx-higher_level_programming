6f4e841e3a9957be38753ede41d6ca598ddfcc62
Demakatso <demakatsomay@gmail.com>
2023-06-08 13:20:15 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-result_add = add(a, b)
-result_sub = sub(a, b)
-result_mul = mul(a, b)
-result_div = div(a, b)
-
-print(f"The addition of {a} and {b} is: {result_add}")
-print(f"The subtraction of {a} and {b} is: {result_sub}")
-print(f"The multiplication of {a} and {b} is: {result_mul}")
-print(f"The division of {a} and {b} is: {result_div}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
