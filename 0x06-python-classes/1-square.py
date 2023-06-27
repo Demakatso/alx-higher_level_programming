40c0070a45212ff70bf4d88035571df29da67ff1
Demakatso <demakatsomay@gmail.com>
2023-06-27 13:39:28 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    ''' The class defines a square. '''
+    def __init__(self, size):
+        '''
+        The __init__ method creates a private attribute \
+        called size and is instantiated to a variable called size.
+
+        Args:
+            size (int): a critical parameter of a square
+        '''
+        self.__size = size
