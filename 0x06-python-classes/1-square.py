2d2e40d78f8c96dfa8783d43a361d22baaa590b1
Demakatso <demakatsomay@gmail.com>
2023-06-27 13:41:50 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''
+This is a module that provides a class definition for a square.
+'''
+
+
 class Square:
     ''' The class defines a square. '''
     def __init__(self, size):
