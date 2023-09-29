c755cba8a8d03aae1e2f72f288f3673bcd91bc38
Demakatso <demakatsomay@gmail.com>
2023-09-29 15:51:04 -0700

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
+"""Displays the X-Request-Id header variable of a request to a given URL
 """
-A script that fetches the content of https://intranet.hbtn.io/status using the requests library
-and displays information about the response.
-"""
+import sys
 import requests
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
     r = requests.get(url)
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print(r.headers.get("X-Request-Id"))
